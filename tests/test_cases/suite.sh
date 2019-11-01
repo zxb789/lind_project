fa51c4e7e85a23b1c0afbde64d6e033bbdcfcbbe
lind <lind@nyu.edu>
2019-11-01 03:35:18 -1000

    Fixed errors in test cases

--- tests/test_cases/suite.sh
+++ tests/test_cases/suite.sh
@@ -1,16 +1,32 @@
+echo "$@"
 echo "Compiling test cases"
 for var in "$@"; do
     echo $var
     varnexe="${var%.*}.nexe";
     x86_64-nacl-gcc-4.4.3 $var -o test_out/$varnexe -std=gnu99;
-    lindfs cp $PWD/test_out/*.nexe
-    rm "test_out/$varnexe"
+    varnonexe="${var%.*}";
+    gcc $var -o test_out/$varnonexe
 done
+lindfs cp $PWD/test_out/
 
 echo "Executing test cases"
 for var in "$@"; do
     nexefile="${var%.*}.nexe";
-    lind "/tests/test_cases/test_out/$nexefile";
-    echo "finished"
-    lindfs rm "/tests/test_cases/test_out/$nexefile";
+    varnonexe="${var%.*}";
+    exec 3>&2
+    exec 2> /dev/null
+    lindoutput=$(lind "/tests/test_cases/test_out/$nexefile");
+    exec 2>&3
+    echo "------------------------------------------------------------------"
+    echo "lindoutput"
+    echo "$lindoutput"
+    regularoutput=$(./test_out/$varnonexe)
+    echo "regularoutput"
+    echo "$regularoutput"
+    echo "Does lindoutput == regularoutput?"
+    [[ "$lindoutput" = "$regularoutput" ]] && echo TEST PASSED || echo TEST FAILED
+    echo $nexefile
 done
+echo "------------------------------------------------------------------"
+rm ./test_out/*
+lindfs deltree "/tests/test_cases/test_out"
