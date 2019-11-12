20d67e6eb155268b8c5f9d28ca9530956ad3c427
lind <lind@nyu.edu>
2019-11-12 10:47:36 -1000

    added suite

--- tests/test_cases/suite.sh
+++ tests/test_cases/suite.sh
@@ -1,7 +1,25 @@
-echo "$@"
-echo "Compiling test cases"
+deterministicinput=()
+nondeterministicinput=()
+declare -n arglist='nondeterministicinput'
 for var in "$@"; do
-    echo $var
+    case "$var" in
+        -d)
+            declare -n arglist='deterministicinput';;
+        -*)
+            echo "Invalid command line argument $var"
+            exit;;
+        *)
+            for arg in $(cat "$var"); do
+                arglist+=("$arg");
+            done;;
+    esac;
+done;
+
+echo ${deterministicinput[@]}
+echo ${nondeterministicinput[@]}
+totalarray=( "${deterministicinput[@]}" "${nondeterministicinput[@]}" )
+echo "Compiling test cases"
+for var in "${totalarray[@]}"; do
     varnexe="${var%.*}.nexe";
     x86_64-nacl-gcc-4.4.3 $var -o test_out/$varnexe -std=gnu99;
     varnonexe="${var%.*}";
@@ -9,8 +27,9 @@ for var in "$@"; do
 done
 lindfs cp $PWD/test_out/
 
-echo "Executing test cases"
-for var in "$@"; do
+echo "Executing nondeterministic test cases"
+echo "------------------------------------------------------------------"
+for var in "${nondeterministicinput[@]}"; do
     nexefile="${var%.*}.nexe";
     varnonexe="${var%.*}";
     exec 3>&2
@@ -25,8 +44,25 @@ for var in "$@"; do
     echo "$regularoutput"
     echo "Does lindoutput == regularoutput?"
     [[ "$lindoutput" = "$regularoutput" ]] && echo TEST PASSED || echo TEST FAILED
-    echo $nexefile
 done
 echo "------------------------------------------------------------------"
+echo "Executing deterministic test cases"
+for var in "${deterministicinput[@]}"; do
+    nexefile="${var%.*}.nexe";
+    varnonexe="${var%.*}";
+    exec 3>&2
+    exec 2> /dev/null
+    lindoutput=$(lind "/tests/test_cases/test_out/$nexefile");
+    exec 2>&3
+    echo "------------------------------------------------------------------"
+    echo "lindoutput"
+    echo "$lindoutput"
+    regularoutput=$(./test_out/$varnonexe)
+    echo "regularoutput"
+    echo "$regularoutput"
+    echo "Does lindoutput fit regular expression modified regularoutput?"
+    python2 "${var%.*}.py" "$lindoutput" "$regularoutput"
+done
+
 rm ./test_out/*
 lindfs deltree "/tests/test_cases/test_out"
