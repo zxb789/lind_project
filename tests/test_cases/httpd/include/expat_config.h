9f0bd80b6c25cbb1d8a7b8d3dd776397699ccd17
Neel Chauhan <nc1682@nyu.edu>
2019-11-22 18:50:17 -0500

    Remove PACKAGE_ #defines in expat_confiig.h

--- tests/test_cases/httpd/include/expat_config.h
+++ tests/test_cases/httpd/include/expat_config.h
@@ -73,24 +73,6 @@
 /* Name of package */
 #define PACKAGE "expat"
 
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "expat-bugs@libexpat.org"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "expat"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "expat 2.2.6"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "expat"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.6"
-
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
